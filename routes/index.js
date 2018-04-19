31f6af0ea7ae55abb3c071eac81ccfb0a20b7a3b
Michael Lande Blau <miclande@gmail.com>
2018-04-19 16:14:24 +0200

    added frontend boileplate code, added axios

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const catService = require("../cat-service")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
+  res.locals.facts = ["Cat fact will be injected here"]
   res.render('index');
 });
 
