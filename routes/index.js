9061b65be8e78d6113d8aea6fe6f3f62aa0c5a05
Michael Lande Blau <miclande@gmail.com>
2018-04-19 16:15:54 +0200

    new branch solution-code

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const catService = require("../cat-service")
-
+//solution 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.locals.facts = ["Cat fact will be injected here"]
